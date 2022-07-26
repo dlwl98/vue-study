fc25749580626a8f176855be2fa383546e003407
dlwl98 <ff981113@hanyang.ac.kr>
2022-07-26 17:54:23 +0900

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+	<h2>About View</h2>
+	<button class="btn btn-primary" @click="goHomePage">Go Home</button>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goHomePage = () => {
+	router.push({
+		name: 'Home',
+	});
+};
+</script>
+
+<style lang="scss" scoped></style>
